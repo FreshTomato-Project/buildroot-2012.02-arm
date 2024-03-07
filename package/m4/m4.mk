561761175ae945ca648d14beba5dbd936d3fb63d
pedro <pedro311@gmail.com>
2024-03-07 21:13:14 +0100

    build: update gmp, m4 and mpfr packages to be able to build the toolchain on Debian 12

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-M4_VERSION = 1.4.18
+M4_VERSION = 1.4.19
 M4_SOURCE = m4-$(M4_VERSION).tar.bz2
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_CONF_ENV = gl_cv_func_gettimeofday_clobber=no
