c52fde3f00c49b66723035c3a54fd8ce7c7d4a2f
pedro <pedro311@gmail.com>
2024-03-07 21:09:47 +0100

    build-toolchain.sh: fix some issues; disable dbg

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-set -e
-set -x
-
 make clean
 make
 
 FILES=./dl_save/files
+HERE=$(pwd)
 TOOLCHAIN=./output/host/hndtools-arm-linux-2.6.36-uclibc-4.5.3
 SYSROOT=$TOOLCHAIN/arm-brcm-linux-uclibcgnueabi/sysroot
 
-ln -s arm-linux/sysroot/usr $TOOLCHAIN/usr
+cd $TOOLCHAIN
+ln -sf arm-linux/sysroot/usr usr
+cd $HERE
 
-rm $TOOLCHAIN/lib/libgmp.*
-rm $TOOLCHAIN/lib/libiberty.a
-rm $TOOLCHAIN/lib/libc.a
+rm -f $TOOLCHAIN/lib/libgmp.*
+rm -f $TOOLCHAIN/lib/libiberty.a
+rm -f $TOOLCHAIN/lib/libc.a
 
-cp $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
-cp $FILES/namespaces/if_link.h $SYSROOT/usr/include/linux/if_link.h
-cp $FILES/namespaces/sysnum.h $SYSROOT/usr/include/bits/sysnum.h
-cp $FILES/namespaces/unistd.h $SYSROOT/usr/include/asm-generic/unistd.h
+cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
+cp -f $FILES/namespaces/if_link.h $SYSROOT/usr/include/linux/if_link.h
+cp -f $FILES/namespaces/sysnum.h $SYSROOT/usr/include/bits/sysnum.h
+cp -f $FILES/namespaces/unistd.h $SYSROOT/usr/include/asm-generic/unistd.h
 
-cp $FILES/newer/ctype.h $SYSROOT/usr/include/ctype.h
-cp $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
-cp $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
-cp $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
+cp -f $FILES/newer/ctype.h $SYSROOT/usr/include/ctype.h
+cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
+cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
+cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
 
-cp $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
+cp -f $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
 
-cp $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
+cp -f $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
 
-echo "Toolchain successfully built!"
+echo -e "\nToolchain successfully built!\n\n"
