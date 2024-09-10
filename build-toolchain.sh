5ed239f5d5c0b40b839225b244c5e82520f70ec1
pedro <pedro311@gmail.com>
2024-09-11 01:02:43 +0200

    uClibc-0.9.33.2: add 0072-timex.h patch

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -22,13 +22,8 @@ ret=$?
 	rm -f $TOOLCHAIN/lib/libc.a
 
 	cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
-	#cp -f $FILES/namespaces/if_link.h $SYSROOT/usr/include/linux/if_link.h
-	#cp -f $FILES/namespaces/sysnum.h $SYSROOT/usr/include/bits/sysnum.h
-	#cp -f $FILES/namespaces/unistd.h $SYSROOT/usr/include/asm-generic/unistd.h
-	#cp -f $FILES/newer/ctype.h $SYSROOT/usr/include/ctype.h
 	cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
 	cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
-	cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
 	cp -f $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
 	cp -f $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
 
