322c616ab7144781d9d8fa5ef0ecfc505eded474
pedro <pedro311@gmail.com>
2024-10-16 13:29:55 +0200

    Simplify toolchain build procedure with different gcc

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -7,9 +7,15 @@ ret=$?
 [ $ret -ne 0 ] && {
 	echo -e "\nAn error occurred while building the toolchain!\n\n"
 } || {
+	[ $(cat .config | grep "BR2_GCC_VERSION=\"5.3.0\"") ] && {
+		# to build, remove mpc-0.9-configure.patc, buildroot-libtool-v2.4.patch and replace .config, gmp.mk, mpc.mk and mpfr.mk with *.mk-5.3 versions
+		TOOLCHAIN=./output/host/hndtools-arm-uclibc-5.3
+	} || {
+		TOOLCHAIN=./output/host/hndtools-arm-linux-2.6.36-uclibc-4.5.3
+	}
+
 	FILES=./dl_save/files
 	HERE=$(pwd)
-	TOOLCHAIN=./output/host/hndtools-arm-linux-2.6.36-uclibc-4.5.3
 	SYSROOT=$TOOLCHAIN/arm-brcm-linux-uclibcgnueabi/sysroot
 
 	cd $TOOLCHAIN
@@ -17,6 +23,7 @@ ret=$?
 	ln -sf arm-linux/sysroot/usr usr
 	cd $HERE
 
+	# still needed?
 	rm -f $TOOLCHAIN/lib/libgmp.*
 	rm -f $TOOLCHAIN/lib/libiberty.a
 	rm -f $TOOLCHAIN/lib/libc.a
